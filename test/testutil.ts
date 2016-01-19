ba68d557f5816da864b8f4e8489168fd3f0110ef
Timo Dörr <timo@latecrew.de>
2016-01-19 20:36:23 +0100

    TSify Testlog class

--- test/testutil.ts
+++ test/testutil.ts
@@ -55,101 +55,103 @@ export function connect_n(n): Array<Promise<Session>> {
 }
 
 
-export var Testlog = function(filename) {
+export class Testlog {
 
-    var self = this;
+    private _filename: string;
+    private _log: Array<any> = [];
 
-    self._filename = filename;
-    self._log = [];
-};
-
-
-Testlog.prototype.log = function() {
-
-    var self = this;
-
-    //console.log.apply(this, arguments);
-    self._log.push(arguments);
-};
-
-
-Testlog.prototype.stringify = function() {
-
-    var self = this;
-
-    var s = '';
-    for (var i = 0; i < self._log.length; ++i) {
-        s += i;
-        let args = self._log[i];
-        for (let arg in args) {
-
-            // stringify with dict attributes ordered
-            s += ' ' + self.stringifyWithOrderedKeys(args[arg]);
-            //s += ' ' + JSON.stringify(args[arg]);
-        }
-        s += "\n";
+    constructor(filename: string) {
+        this._filename = filename;
     }
-    return s;
-};
 
-Testlog.prototype.stringifyWithOrderedKeys = function(arg) {
-    var self = this;
-    if (arg != null && typeof (arg) == "object") {
-        var clazz = Object.prototype.toString.call(arg).toLowerCase();
-        if (clazz.indexOf("array") != -1) {
-            var retval = "[";
-            for (var i = 0; i < arg.length; i++) {
-                if (i > 0) retval += ',';
-                retval += self.stringifyWithOrderedKeys(arg[i]);
-            }
-            retval += "]";
-            return retval;
-        } else {
-            var retval = "{";
-            var keys = Object.keys(arg).sort();
-            for (var i = 0; i < keys.length; i++) {
-                if (i > 0) retval += ',';
-                retval += '"' + keys[i] + '":' + self.stringifyWithOrderedKeys(arg[keys[i]]);
-            }
-            retval += "}";
-            return retval;
-        }
-    } else {
-        return JSON.stringify(arg);
+
+    log(...args: Array<any>) {
+
+        var self = this;
+
+        //console.log.apply(this, arguments);
+        self._log.push(arguments);
     }
-};
 
 
-Testlog.prototype.check = function() {
+    stringify() {
 
-    var self = this;
-    var slog = self.stringify();
+        var self = this;
 
-    if (global === undefined) {
-        // running in browser, fetch log via synchronous ajax
-        let xhr = new XMLHttpRequest();
-        xhr.open("GET", self._filename, false);
-        xhr.send(null);
-        let slog_baseline = xhr.responseText;
-        if (slog != slog_baseline) {
-            return "\nExpected:\n\n" + slog_baseline + "\n\n\nGot:\n\n" + slog + "\n\n";
-        } else {
-            return null;
+        var s = '';
+        for (var i = 0; i < self._log.length; ++i) {
+            s += i;
+            let args = self._log[i];
+            for (let arg in args) {
+
+                // stringify with dict attributes ordered
+                s += ' ' + self.stringifyWithOrderedKeys(args[arg]);
+                //s += ' ' + JSON.stringify(args[arg]);
+            }
+            s += "\n";
         }
-    } else {
-        // running in node, use fs module
-        let fs = require("fs");
-        if (fs.existsSync(self._filename)) {
-            let slog_baseline = fs.readFileSync(self._filename);
+        return s;
+    };
+
+    stringifyWithOrderedKeys(arg) {
+        var self = this;
+        if (arg != null && typeof (arg) == "object") {
+            var clazz = Object.prototype.toString.call(arg).toLowerCase();
+            if (clazz.indexOf("array") != -1) {
+                var retval = "[";
+                for (var i = 0; i < arg.length; i++) {
+                    if (i > 0) retval += ',';
+                    retval += self.stringifyWithOrderedKeys(arg[i]);
+                }
+                retval += "]";
+                return retval;
+            } else {
+                var retval = "{";
+                var keys = Object.keys(arg).sort();
+                for (var i = 0; i < keys.length; i++) {
+                    if (i > 0) retval += ',';
+                    retval += '"' + keys[i] + '":' + self.stringifyWithOrderedKeys(arg[keys[i]]);
+                }
+                retval += "}";
+                return retval;
+            }
+        } else {
+            return JSON.stringify(arg);
+        }
+    }
+
+
+    check() {
+
+        var self = this;
+        var slog = self.stringify();
+
+        if (global === undefined) {
+            // running in browser, fetch log via synchronous ajax
+            let xhr = new XMLHttpRequest();
+            xhr.open("GET", self._filename, false);
+            xhr.send(null);
+            let slog_baseline = xhr.responseText;
             if (slog != slog_baseline) {
                 return "\nExpected:\n\n" + slog_baseline + "\n\n\nGot:\n\n" + slog + "\n\n";
             } else {
                 return null;
             }
         } else {
-            fs.writeFileSync(self._filename, slog);
-            console.log("Know-good log file created", self._filename, slog.length);
-            return null;
+            // running in node, use fs module
+            let fs = require("fs");
+            if (fs.existsSync(self._filename)) {
+                let slog_baseline = fs.readFileSync(self._filename);
+                if (slog != slog_baseline) {
+                    return "\nExpected:\n\n" + slog_baseline + "\n\n\nGot:\n\n" + slog + "\n\n";
+                } else {
+                    return null;
+                }
+            } else {
+                fs.writeFileSync(self._filename, slog);
+                console.log("Know-good log file created", self._filename, slog.length);
+                return null;
+            }
         }
     }
 }
